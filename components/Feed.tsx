4b43346848bf2ec8eaed733417ad362c6aa9d047
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-13 22:47:56 +0530

    Added Toast for Feed refresh

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,17 +3,33 @@ import { RefreshIcon } from '@heroicons/react/outline'
 import Tweetbox from "./Tweetbox";
 import { Tweet } from "../typings";
 import TweetComponent from './Tweet'
+import { fetchTweets } from "../utils/fetchTweets";
+import { useState } from 'react'
+import toast from "react-hot-toast";
 
 interface Props {
     tweets: Tweet[]
 }
 
-const Feed = ({ tweets }: Props) => {
+const Feed = ({ tweets: TweetsProp }: Props) => {
+    const [tweets, setTweets] = useState<Tweet[]>(TweetsProp);
+    
+    const handleRefresh =async () => {
+        const refreshToast = toast.loading("Refreshing....")
+        
+        const tweets = await fetchTweets();
+        setTweets(tweets);
+
+        toast.success("Feed Updated!!", {
+            id: refreshToast
+        })
+    }
+    
     return (
         <div className="col-span-7 lg:col-span-5 border-x">
             <div className="flex justify-between items-center">
                 <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-                <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+                <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" onClick={handleRefresh}/>
             </div>
 
             {/* Tweetbox */}
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -52,7 +52,7 @@ const Tweet = ({ tweet }: Props) => {
             <div className='flex justify-between mt-5'>
                 <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
                     <ChatAlt2Icon className='h-5 w-5' />
-                    <p>5</p>
+                    <p>{comments.length}</p>
                 </div>
 
                 <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
@@ -71,9 +71,9 @@ const Tweet = ({ tweet }: Props) => {
             {comments.length > 0 && (
                 <div className='my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5'>
                     {comments.map((comment) => (
-                        <div key={comment._id} className='flex space-x-2'>
-                            <hr className='absolute '/>
-                            <img className='h-7 w-7 object-cover rounded-full' src={comment.profileImg} alt='comment-pic' />
+                        <div key={comment._id} className='relative flex space-x-2'>
+                            <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30'/>
+                            <img className='mt-2 h-7 w-7 object-cover rounded-full' src={comment.profileImg} alt='comment-pic' />
                             <div>
                                 <div className='flex items-center space-x-1'>
                                     <p className='mr-1 font-bold'>{comment.username}</p>

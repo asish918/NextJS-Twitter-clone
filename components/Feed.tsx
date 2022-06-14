8f3d3a79d3251edc75617c18f4e88b5fff4f956c
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-14 22:13:12 +0530

    Bug Fixes

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -26,7 +26,7 @@ const Feed = ({ tweets: TweetsProp }: Props) => {
     }
     
     return (
-        <div className="col-span-7 lg:col-span-5 border-x">
+        <div className="col-span-7 lg:col-span-5 border-x max-h-screen overflow-scroll scrollbar-hide">
             <div className="flex justify-between items-center">
                 <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
                 <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" onClick={handleRefresh}/>

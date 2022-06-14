0ad4319ed8d8b6ab88f5bec316daff94cfa97853
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-14 12:08:48 +0530

    Added Image URL Functionality

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -5,14 +5,31 @@ import {
     PhotographIcon,
     SearchCircleIcon
 } from '@heroicons/react/outline'
-import { useState } from 'react'
+import { useSession } from 'next-auth/react';
+import { format } from 'path';
+import { useRef, useState } from 'react'
 
 const Tweetbox = () => {
     const [input, setInput] = useState<string>('');
+    const [image, setImage] = useState<string>('');
+    const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
+
+    const imageInputRef = useRef<HTMLInputElement>(null);
+    const { data: session } = useSession();
+
+    const addImageTweet = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        if(!imageInputRef.current?.value) return;
+
+        setImage(imageInputRef.current.value)
+        imageInputRef.current.value = ''
+        setImageUrlBoxIsOpen(false);
+    }
 
     return (
         <div className='flex space-x-2 p-5'>
-            <img className="h-14 w-14 object-cover rounded-full mt-4" src='https://picsum.photos/seed/200/200' alt="profile-pic" />
+            <img className="h-14 w-14 object-cover rounded-full mt-4" src={session?.user?.image || 'https://picsum.photos/seed/200/200'} alt="profile-pic" />
 
             <div className='flex flex-1 items-center pl-2'>
                 <form className='flex flex-1 flex-col'>
@@ -20,14 +37,26 @@ const Tweetbox = () => {
                     <div className='flex items-center'>
                         <div className='flex space-x-2 text-twitter flex-1'>
                             {/* Icons */}
-                            <PhotographIcon className='h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease-out hover:scale-150' />
+                            <PhotographIcon onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)} className='h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease-out hover:scale-150' />
                             <SearchCircleIcon className='h-5 w-5' />
                             <EmojiHappyIcon className='h-5 w-5' />
                             <CalendarIcon className='h-5 w-5' />
                             <LocationMarkerIcon className='h-5 w-5' />
                         </div>
-                        <button disabled={!input} className='bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40'>Tweet</button>
+                        <button disabled={!input || !session} className='bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40'>Tweet</button>
                     </div>
+
+                    {imageUrlBoxIsOpen && (
+                        <form className='mt-5 flex rounded-lg bg-twitter/80 py-2 px-4'>
+                            <input ref={imageInputRef} className='flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white' type='text' placeholder='Enter Image URL'/>
+                            <button type='submit' onClick={addImageTweet} className='font-bold text-white'>Add Image</button>
+                        </form>
+                    )}
+
+                    {image && (
+                        <img className='mt-10 h-40 w-full rounded-xl object-contain shadow-lg' src={image} alt='tweet-image' />
+                    )
+                    }
                 </form>
             </div>
         </div>
